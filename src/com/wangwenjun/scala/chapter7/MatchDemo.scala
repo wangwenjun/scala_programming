2f41f437943fec205e76b4a6a71f43a7e013d162
wangwenjun <532500648@qq.com>
2017-06-11 22:37:07 +0800

    match example

---
+++ src/com/wangwenjun/scala/chapter7/MatchDemo.scala
@@ -0,0 +1,12 @@
+package com.wangwenjun.scala.chapter7
+
+/** *************************************
+  *
+  * @author:Alex Wang
+  * @Date:2017 /6/11
+  *            QQ:532500648
+  *            QQ交流群:286081824
+  * **************************************/
+object MatchDemo {
+
+}
--- src/com/wangwenjun/scala/chapter7/TryCatch.scala
+++ src/com/wangwenjun/scala/chapter7/TryCatch.scala
@@ -2,4 +2,39 @@ package com.wangwenjun.scala.chapter7
 
 object TryCatch extends App {
 
+  val n = 11
+
+  try {
+    val half = if (n % 2 == 0) n / 2 else throw new RuntimeException("n must be even")
+    println(half)
+  } catch {
+    case ex: RuntimeException => ex.printStackTrace()
+    case ex: Throwable => println("Other")
+  }
+
+  println("=================================")
+
+  var cause: Throwable = _
+
+  try {
+    1 / 0
+  } catch {
+    case e: RuntimeException => println(e); cause = e
+    case e: Throwable => println(e); cause = e
+  } finally {
+    println("=========finally============")
+  }
+
+  cause.printStackTrace()
+  println("=================================")
+  val v = try {
+    1 / 0
+  } catch {
+    case e: RuntimeException => -1
+    case e: Throwable => -2
+  } finally {
+     5
+  }
+
+  println(v)
 }
